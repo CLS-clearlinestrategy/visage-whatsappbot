4ec7bed20d65e1f9c5972900c8565bb6b07daf4d
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2026-05-27 01:40:34 -0300

    init

---
+++ src/main/java/com/whatsapp_bot/ravius/RaviusApplication.java
@@ -0,0 +1,13 @@
+package com.whatsapp_bot.ravius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaviusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaviusApplication.class, args);
+	}
+
+}
